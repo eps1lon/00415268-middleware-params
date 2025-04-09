cb7a89698a393deee0eb89a219fd0d04ad892792
Sebastian Sebbie Silbermann <sebastian.silbermann@vercel.com>
2025-04-09 13:56:30 +0200

    Repro on Canary

--- middleware.ts
+++ middleware.ts
@@ -3,38 +3,26 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-	const hostName = request.nextUrl.hostname;
+  const url = request.nextUrl.clone();
+  console.log("url", url);
+  url.port = "";
+  url.hostname = "00415268-middleware-params.preview.vercel-support.app";
+  console.log("fixed url", url);
+  console.log("url.href", url.href);
+  console.log("url.toString()", url.toString());
 
-	if (
-		hostName ===
-		"00415268-middleware-params-git-main.preview.vercel-support.app" || hostName === 'localhost'
-	) {
-		const url = request.nextUrl.clone();
-		console.log("url", url);
-		url.hostname = "00415268-middleware-params.preview.vercel-support.app";
-		console.log("fixed url", url);
-		console.log("url.href", url.href);
-		console.log("url.toString()", url.toString());
-		return new Response(null, {
-			headers: {
-				location: url.href.replace(/\:\d+/, '')
-			},
-			status: 307
-		});
-	}
-
-	return NextResponse.next();
+  return NextResponse.redirect(url, 307);
 }
 
 export const config = {
-	matcher: [
-		/*
-		 * Match all request paths except for the ones starting with:
-		 * - api (API routes)
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-		 */
-		"/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-	],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  redirects: async () => {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://00415268-middleware-params.preview.vercel-support.app/:path*', // new domain
-        statusCode: 307,
-        missing: [
-          {
-            'value': '00415268-middleware-params.preview.vercel-support.app', // old domain
-            'type': 'host'
-          }
-        ]
-      }
-    ]
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
